d3de3eb2178f276a89b406e67c373cf137e4a1ba
aminekebouche <keboucheamine6@gmail.com>
2023-07-25 12:09:11 +0200

    finish

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,36 @@
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import { TiDelete } from 'react-icons/ti';
 
+const ExpenseItem = (props) => {
+    const { dispatch } = useContext(AppContext);
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.expense.id,
+        });
+    };
+
+    const increaseAllocation = () => {
+        const expense = {
+            name: props.expense.name,
+            cost: 10,
+        };
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+    }
+
+  
+    return (
+        <tr>
+        <td>{props.expense.name}</td>
+        <td>{props.expense.cost} €</td>
+        <td><button onClick={increaseAllocation}>+</button></td>
+        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        </tr>
+    );
+}
+
+export default ExpenseItem
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,16 @@
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
+
+
+const Remaining = () => {
+    const {remaining}  =useContext(AppContext);
+    const alertType = remaining > 0 ? "alert-success" : "alert-danger"
+  return (
+    <div className={`alert ${alertType}`}>
+            <span>Remaining: {remaining}€</span>
+    </div>
+  )
+}
+
+export default Remaining
